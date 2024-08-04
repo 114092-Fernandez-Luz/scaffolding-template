61470d89e10370d9d275191cada82f28ebee0eb4
114092_Fernandez <114092@tecnicatura.frc.utn.edu.ar>
2024-08-04 15:53:29 -0300

    se agrega el controller de ping

---
+++ src/main/java/ar/edu/utn/frc/tup/lciii/controllers/PingController.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.frc.tup.lciii.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    /**
+     * This method is used to check if the server is up and running.
+     *
+     
+     */
+    @GetMapping("/ping")
+    public String pong() {
+        return "pong";
+    }
+}
